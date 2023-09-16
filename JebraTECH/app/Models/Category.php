e0a0f680bdc021553f0adb668c42221bc029528e
jabrallah <mohammed01110834150@gmail.com>
2023-09-16 08:39:07 +0300

    category bage

--- JebraTECH/app/Models/Category.php
+++ JebraTECH/app/Models/Category.php
@@ -4,16 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 
 class Category extends Model
 {
     use  HasFactory, Notifiable;
+    use SoftDeletes;
+
     protected $fillable = [
         'name',
     ];
 
     public function blogs(){
         return $this->hasMany(Blog::class,'category_id');
+    }
+
+    public function delete()
+    {
+        $this->blogs()->each(function ($blog) {
+            $blog->delete();
+        });
+
+        return parent::delete();
+    }
+
+    public function restore()
+    {
+        $this->blogs()->each(function ($blog) {
+            $blog->restore();
+        });
+
+        return parent::restore();
     }
 }
